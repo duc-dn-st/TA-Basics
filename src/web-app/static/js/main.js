7d4b67f0254db0761a94c5b83521dbf106638e9a
sys_pc03 <system.mobile.robot@gmail.com>
2024-01-09 09:36:41 +0900

    :rabbiit: refactor main

--- src/web-app/static/js/main.js
+++ src/web-app/static/js/main.js
@@ -2,3 +2,120 @@ function redirect_to_mapping () {
     location.replace("/mapping")
     console.log("redirect")
 };
+
+$(document).ready(function() {
+
+    $body = $("body");
+
+    $(".close-navigation").click(function(event) {
+        event.stopPropagation();
+        // console.log(event.target.previousSibling.data)
+        $.ajax({
+            url: '/deletemap',
+            type: 'POST',
+            data: event.target.previousSibling.data,
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        })
+
+    });
+
+    var close = document.getElementsByClassName("close-navigation");
+    var i;
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+
+    $("#index-list").click(function(event) {
+        
+        document.cookie = event.target.innerHTML;
+        
+        $('#exampleModal').modal('hide');
+
+        $.ajax({
+            url: '/main/navigation-precheck',
+            type: 'GET',
+            success: function(response) {
+
+                if (response.mapcount > 0) {
+
+                    // $body.addClass("loading");
+
+                    $.ajax({
+
+                        url: '/main/gotonavigation',
+                        type: 'POST',
+                        data: event.target.innerHTML,
+                        success: function(response) {
+                            function connect() {
+
+                                var ros = new ROSLIB.Ros({
+                                    url: 'ws://localhost:9090'
+                                });
+
+                                ros.on('connection', function() {
+
+                                    console.log('Connected to websocket server.');
+                                    var rosTopic = new ROSLIB.Topic({
+                                        ros: ros,
+                                        name: '/rosout_agg',
+                                        messageType: 'rosgraph_msgs/Log'
+                                    });
+
+                                    rosTopic.subscribe(function(message) {
+
+                                        if (message.msg == "odom received!") {
+                                            console.log(message.msg)
+                                            window.location = "/navigation";
+                                            $body.removeClass("loading");
+                                            rosTopic.unsubscribe();
+                                        }
+                                    });
+
+                                });
+
+
+                                ros.on('close', function() {
+                                    console.log('Connection to websocket server closed.');
+                                });
+
+
+                                ros.on('error', function(error) {
+                                    console.log('Error connecting to websocket server: ', error);
+                                    setTimeout(function() {
+                                        connect();
+                                    }, 1000);
+                                });
+                            }
+
+                            connect();
+
+
+                        },
+                        error: function(error) {
+                            console.log(error);
+                        }
+
+                    })
+
+
+                } else {
+                    alert("No map in directory.Please do mapping.")
+                }
+            },
+            error: function(error) {
+                console.log(error);
+            }
+
+        })
+    });
+
+});
+
